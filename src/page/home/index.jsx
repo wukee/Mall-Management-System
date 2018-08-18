7ba0d01b146a6e51dcb2fc58de90c5d220ba04cb
wk <120907221@qq.com>
2018-08-18 09:14:23 +0800

    home页搭建

---
+++ src/page/home/index.jsx
@@ -0,0 +1,18 @@
+/**
+ * @Author: wuke
+ * @Date: 2018/8/17
+ * @Last Modified by: wuke
+ * @Last Modified time: 2018/8/17
+ */
+
+import React from 'react'
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div>Home</div>
+        )
+    }
+}
+
+export default Home
